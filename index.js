235bb0a6c8b695b2d2fb8d76c40afac5f2bc7ce4
Manolis Maragkoudakis <manolis.marag@gmail.com>
2020-04-28 01:03:03 +0300

    fixed port

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 var sqlite3 = require('sqlite3').verbose();
 let books = [];
-
-app.listen(3000, function() {
-	 console.log('listening at port 3000');
+var port = process.env.PORT;
+app.listen(port, function() {
+	 console.log('listening at port ${port}');
 });
 app.use(express.static('books'));
 app.use(express.json());
